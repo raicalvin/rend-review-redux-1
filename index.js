03483af34557e959513b6a43d4f051a614f3b58b
Calvin Rai <raicalvin@gmail.com>
2018-10-28 14:51:08 -0700

    Added a return function to subscribe() so that the user can unsubscribe from the listener they passed into subscribe() if they choose to.

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ function createStore() {
   // Every time subscribe() is invoked, the function passed to it will be stored in this array:
   let listeners = [];
 
-  // This method is responsible for
+  // ITEM 3: Listen for changes of the state using the subscribe() function
+
+  // This method is responsible for taking the listner passed into it and managing it
   const subscribe = listener => {
-    listeners.push(listener); // push the listener passed to subscribe() from user into the listeners array
+    // push the listener passed to subscribe() from user into the listeners array:
+    listeners.push(listener);
+
+    /*
+        Now we want to create a way for the user to unsubscribe from certain subscribe calls. Return a function to the user that let's them do this. When the user invokes this function, call the filter() function to filter out the listener they want to unsibscribe from, in this case 'listener':
+    */
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
   };
 
   // Whenever createStore() is invoked, we want to return an object where one of the properties on this object will allow the user to get the state
