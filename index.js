4552c5e59546de45c23f17bbd2bd113260923d51
Calvin Rai <raicalvin@gmail.com>
2018-10-28 14:34:24 -0700

    Created a subscribe() function that will take in a listener function passed in from the user and then store it in the listeners array.

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ function createStore() {
   // ITEM 2: Returning the state of the application when requested.
   const getState = () => state;
 
+  // Every time subscribe() is invoked, the function passed to it will be stored in this array:
+  let listeners = [];
+
+  // This method is responsible for
+  const subscribe = listener => {
+    listeners.push(listener); // push the listener passed to subscribe() from user into the listeners array
+  };
+
   // Whenever createStore() is invoked, we want to return an object where one of the properties on this object will allow the user to get the state
   return {
     getState
