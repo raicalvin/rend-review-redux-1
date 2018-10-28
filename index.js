c3a28410901cdac54c683b97f8c57d6765d3944d
Calvin Rai <raicalvin@gmail.com>
2018-10-28 15:53:23 -0700

    Modified the code to lay out the library code and the user code, similar to the Redux library.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-// This is a pure function that takes in the state and the action that occurs and returns a new state
-function todos(state = [], action) {
-  if (action.todo === "ADD_TODO") {
-    // modify the state by concatenating the todo to it
-    // .concat() returns us a NEW array
-    return state.concat([action.todo]);
-  }
-  return state;
-}
+/*
+    LIBRARY CODE ====================================================
+*/
 
 // This function will create a brand new store
-function createStore() {
+function createStore(reducer) {
   // The store will have four parts:
   // 1. The state
   // 2. Getting the state
@@ -32,8 +26,8 @@ function createStore() {
     // push the listener passed to subscribe() from user into the listeners array:
     listeners.push(listener);
     /*
-        Now we want to create a way for the user to unsubscribe from certain subscribe calls. Return a function to the user that let's them do this. When the user invokes this function, call the filter() function to filter out the listener they want to unsibscribe from, in this case 'listener':
-    */
+          Now we want to create a way for the user to unsubscribe from certain subscribe calls. Return a function to the user that let's them do this. When the user invokes this function, call the filter() function to filter out the listener they want to unsibscribe from, in this case 'listener':
+      */
     return () => {
       listeners = listeners.filter(l => l !== listener);
       // The user can store this returned function into a variable when they call store.subscribe(...) and invoke it to remove the listener they passed into the subscribe() method call.
@@ -42,8 +36,8 @@ function createStore() {
 
   // ITEM 4: The dispatch() function is responsible for updating the state in our store using the action that occured
   const dispatch = action => {
-    // dispatch() can call the pure reducer function, todos(), passing it the current state and the action
-    state = todos(state, action);
+    // dispatch() can call the pure reducer function, reducer() that was passed into createStore() by the user, passing it the current state and the action
+    state = reducer(state, action);
 
     // Since the state potentially changed, make sure to loop through the listeners and invoke each one of them
     listeners.forEach(listener => listener());
@@ -56,3 +50,20 @@ function createStore() {
     dispatch
   };
 }
+
+/*
+    APP CODE ====================================================
+*/
+
+// This is a pure function that takes in the state and the action that occurs and returns a new state
+function todos(state = [], action) {
+  if (action.todo === "ADD_TODO") {
+    // modify the state by concatenating the todo to it
+    // .concat() returns us a NEW array
+    return state.concat([action.todo]);
+  }
+  return state;
+}
+
+// We can create a store by calling createStore() and passing in the reducer pure function above
+const store = createStore(todos);
